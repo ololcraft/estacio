da8354d37c7bdf3527e54b45950e82097c3df02e
ololcraft <huehueguy01@gmail.com>
2025-05-03 14:33:16 +0000

    adicione do no bispo e switch ln 48 ate ln 98

--- xadres/xadres.c
+++ xadres/xadres.c
@@ -3,7 +3,7 @@ int main(){
     int torre, bispo, rainha;
     int tx, ty, bx, by, rx, ry;
     int escolha;
-    while ((escolha >= 4) || (escolha <= 0) || (torre == 3))
+    while ((escolha >= 4) || (escolha <= 0) || (torre == 3) || (bispo == 5))
     {    
         printf("digite a peca que ira mover\n");
         printf("1. torre\n2. bispo\n3.rainha\n");
@@ -45,7 +45,57 @@ int main(){
                 } while ((torre <= 0) || (torre > 3));
             break;
             case 2:
-            //bispo
+                do
+                {                
+                    printf("escolha a direcao que o bispo ira mover\n");
+                    printf("1. esquerda cima\n2. esquerda baixo\n3. direita cima\n4. direita baixo\n5. voltar\n");
+                    scanf("%d", &bispo);
+                    switch (bispo)
+                    {
+                        case 1:
+                            do
+                            {
+                                printf("digite a quantidade de casas o bispo se movera entre 1 e 8\n");
+                                scanf("%d", &bx);
+                                by = bx;
+                            } while ((bx > 8) || (bx <= 0));
+                            printf("o bispo moveu %d casas pra esquerda e %d casas pra cima\n", bx, by);
+                        break;
+                        case 2:
+                            do
+                            {
+                                printf("digite a quantidade de casas o bispo se movera entre 1 e 8\n");
+                                scanf("%d", &bx);
+                                by = bx;
+                            } while ((bx > 8) || (bx <= 0));                        
+                            printf("o bispo moveu %d casas pra esquerda e %d casas pra baixo\n", bx, by);
+                        break;
+                        case 3:
+                            do
+                            {
+                                printf("digite a quantidade de casas o bispo se movera entre 1 e 8\n");
+                                scanf("%d", &bx);
+                                by = bx;
+                            } while ((bx > 8) || (bx <= 0));                        
+                            printf("o bispo moveu %d casas pra direita e %d casas pra cima\n", bx, by);
+                        break;
+                        case 4:
+                            do
+                            {
+                                printf("digite a quantidade de casas o bispo se movera entre 1 e 8\n");
+                                scanf("%d", &bx);
+                                by = bx;
+                            } while ((bx > 8) || (bx <= 0));                        
+                            printf("o bispo moveu %d casas pra direita e %d casas pra baixo\n", bx, by);
+                        break;
+                        case 5:
+                            printf("voltando...\n");
+                        break;
+                        default:
+                            printf("opção invalida\n");
+                        break;
+                    }
+                } while ((bispo <= 0) || (bispo > 5));
             break;
             case 3:
             //rainha
