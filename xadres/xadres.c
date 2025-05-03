9f1e41ceddefb5966f5d11cb2ff1444519108b15
ololcraft <huehueguy01@gmail.com>
2025-05-03 10:25:22 +0000

    copie e colei do studio

--- xadres/xadres.c
+++ xadres/xadres.c
@@ -1,71 +1,60 @@
 #include <stdio.h>
 int main(){
     int torre, bispo, rainha;
-    int direcaotorre, direcaobispo, direcaorainha;
-    int casastorre, casasbispo, casasrainha;
+    int tx, ty, bx, by, rx, ry;
     int escolha;
-    while (escolha >=3 || escolha <= 0 || direcaotorre == 5)
-    {
-    printf("digite a peca que ira mover\n");
-    printf("1. torre\n2. bispo\n3.rainha\n");
-    scanf("%d", &escolha);
-    switch (escolha)
-    {
-    case 1:
-        printf("escolha a diraçao da torre\n");
-        printf("1. cima\n2. baixo\n3. direita\n4. esquerda\n5. voltar\n");
-        scanf("%d", &direcaotorre);
-        switch (direcaotorre)
-        {
-        case 1:
-            do
-            {
-            printf("digite quantas casas a torre ira mover de 1 a 8\n");
-            scanf("%d", &casastorre);
-            } while (casastorre <= 0 || casastorre > 8);
-            printf("sua torre moveu pra cima %d casas\n", casastorre);
+    while ((escolha >= 4) || (escolha <= 0) || (torre == 3))
+    {    
+        printf("digite a peca que ira mover\n");
+        printf("1. torre\n2. bispo\n3.rainha\n");
+        scanf("%d", &escolha);
+        switch (escolha){
+            case 1:
+                do
+                {                
+                    printf("digite a direcao que a torre ira mover\n");
+                    printf("1. horizontal\n2. vertical\n3. voltar\n");
+                    scanf("%d", &torre);
+                    switch (torre)
+                    {
+                        case 1:
+                            do
+                            {                            
+                                printf("digite a quantidade de casas a torre se movera entre 1 e 8\n");
+                                scanf("%d", &tx);
+                            } while ((tx > 8) || (tx <= 0));
+
+                            printf("voce moveu a torre horizontalmente %d casas\n", tx);
+                        break;
+                        case 2:
+                            do
+                            {   
+                                printf("digite a quantidade de casas a torre se movera entre 1 e 8\n");
+                                scanf("%d", &ty);
+                            } while ((ty > 8) || (ty <= 0));
+
+                            printf("voce moveu a torre verticalmente %d casas\n", ty);    
+                        break;
+                        case 3:
+                        printf("voltando...\n");
+                        break;    
+                        default:
+                        printf("opcao invalida");
+                        break;
+                    }
+                } while ((torre <= 0) || (torre > 3));
             break;
-        case 2:
-            do
-            {
-            printf("digite quantas casas a torre ira mover de 1 a 8\n");
-            scanf("%d", &casastorre);
-            } while (casastorre <= 0 || casastorre > 8);
-            printf("sua torre moveu pra baixo %d casas\n", casastorre);        
+            case 2:
+            //bispo
             break;
-        case 3:
-            do
-            {
-            printf("digite quantas casas a torre ira mover de 1 a 8\n");
-            scanf("%d", &casastorre);
-            } while (casastorre <= 0 || casastorre > 8);
-            printf("sua torre moveu pra direita %d casas\n", casastorre);             
+            case 3:
+            //rainha
+            break;    
+            default:
+            printf("opcao invalida\n");
             break;
-        case 4:
-            do
-            {
-            printf("digite quantas casas a torre ira mover de 1 a 8\n");
-            scanf("%d", &casastorre);
-            } while (casastorre <= 0 || casastorre > 8);
-            printf("sua torre moveu pra esquerda %d casas\n", casastorre); 
-            break;
-        case 5:
-            printf("voltando...\n");
-            break;
-        default:
-            printf("opçao invalida\n");
-            break;
-        }
-        break;
-    case 2:
-        //bispo
-        break;
-    case 3:
-        //rainha
-        break;    
-    default:
-        printf("opcao invalida\n");
-        break;
-    }}
+        }   
+        
+    }
     return 0;
 }
